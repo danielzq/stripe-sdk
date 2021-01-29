f0f537e7fb424d25330a90438215f9713c878034
DanialZhang <daniel.zhang@toyota.com>
2021-01-29 10:38:30 +0800

    update card warning

--- lib/src/models/card.dart
+++ lib/src/models/card.dart
@@ -54,9 +54,9 @@ class StripeCard {
     if (cvc == null) return false;
     if (number != null) {
       final cardType = _ccValidator.validateCCNum(number).ccType;
-      return _ccValidator.validateCVV(cvc, cardType: cardType).isValid;
+      return _ccValidator.validateCVV(cvc, cardType).isValid;
     } else {
-      return _ccValidator.validateCVV(cvc).isValid;
+      return false;
     }
   }
 
